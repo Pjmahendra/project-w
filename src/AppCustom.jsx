8534f07a7ba273a1e3e20296095f88e6da2063a3
Mahendra Pj <pjmahendravk@gmail.com>
2025-09-11 08:44:17 -0700

    Update AppCustom.jsx

--- src/AppCustom.jsx
+++ src/AppCustom.jsx
@@ -81,7 +81,7 @@ function AppCustom() {
         <h1 style={{alignItems:'center',justifyContent:'center',textAlign:'center',fontSize:'50px',fontWeight:"bolder", padding:0}}>
           HAPPY BIRTHDAY 🎂
         </h1>
-        <p style={{textAlign:"center" ,padding:0}}>to my favaorite personperson❣️</p>
+        <p style={{textAlign:"center" ,padding:0}}>to my favaorite person❣️</p>
         <p style={{textAlign:'center',fontWeight:'300',fontSize:'2rem'}}>How lucky you are being a wonderful, amazing, and an absolutely incredible person!</p>
       </div>
 
